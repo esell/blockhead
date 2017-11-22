7aeb94122c8da5d25b96fcf8697b82debe1318d8
esell <esell@esheavyindustries.com>
2017-11-22 10:56:09 -0700

    stubbing out REST endpoints

--- main.go
+++ main.go
@@ -3,73 +3,89 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	myChain := Blockchain{}
-	log.Println(myChain)
 
 	log.Println("creating genesis block...")
 	genBlock, err := myChain.NewBlock(1, 100, true)
 	if err != nil {
-		log.Println(err)
-	}
-	genBlockJSON, err := json.Marshal(genBlock)
-	if err != nil {
-		log.Println("error with JSON: ", err)
+		log.Fatal("unable go create genesis block: ", err)
 	}
 	myChain.AddBlock(genBlock)
-	log.Println("genesis block: ", string(genBlockJSON))
-	returnChain(&myChain)
-	returnLastBlock(&myChain)
+	log.Println("genesis block created, have fun!")
 
-	// create new transaction
-	tempTrans := myChain.NewTransaction("blahfrom", "blahto", 666)
-	log.Println("returned index: ", tempTrans)
-	returnChain(&myChain)
-	returnLastBlock(&myChain)
+	http.Handle("/list", blockListHandler(myChain))
+	http.Handle("/mine", mineHandler(myChain))
+	http.Handle("/newTransaction", newTransactionHandler(myChain))
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
-	for i := 0; i < 4; i++ {
+func blockListHandler(b Blockchain) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "method not supported", http.StatusMethodNotAllowed)
+			return
+		}
+		chainJSON, err := json.Marshal(b)
+		if err != nil {
+			log.Println("error with JSON: ", err)
+		}
+		log.Println("full chain: ", string(chainJSON))
+		w.Write(chainJSON)
+	})
+}
+
+func mineHandler(b Blockchain) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "method not supported", http.StatusMethodNotAllowed)
+			return
+		}
 		// start mining
 		log.Println("starting to mine...")
-		lastProof := myChain.LastBlock()
-		newProof := myChain.ProofOfWork(lastProof.Header.Proof)
+		lastProof := b.LastBlock()
+		newProof := b.ProofOfWork(lastProof.Header.Proof)
 		log.Println("new proof: ", newProof)
 
 		// add new block
-		myNewBlock, err := myChain.NewBlock(myChain.LastBlock().Header.Index+1, newProof, false)
+		newBlock, err := b.NewBlock(b.LastBlock().Header.Index+1, newProof, false)
 		if err != nil {
 			log.Println(err)
-			break
 		}
-		myChain.AddBlock(myNewBlock)
-	}
-	// print new chain
-	returnChain(&myChain)
-	returnLastBlock(&myChain)
+		b.AddBlock(newBlock)
+		blockJSON, err := json.Marshal(newBlock)
+		if err != nil {
+			log.Println("error with JSON: ", err)
+		}
+		log.Println("new block: ", string(blockJSON))
+		w.Write(blockJSON)
+	})
 }
 
-func returnChain(chain *Blockchain) {
-	log.Println()
-	log.Println("****************************************")
-	chainJSON, err := json.Marshal(chain)
-	if err != nil {
-		log.Println("error with JSON: ", err)
-	}
-	log.Println("full chain: ", string(chainJSON))
-	log.Println("****************************************")
-	log.Println()
-}
-
-func returnLastBlock(chain *Blockchain) {
-	log.Println()
-	log.Println("****************************************")
-	chainJSON, err := json.Marshal(chain.LastBlock())
-	if err != nil {
-		log.Println("error with JSON: ", err)
-	}
-	log.Println("last block: ", string(chainJSON))
-	log.Println("****************************************")
-	log.Println()
+func newTransactionHandler(b Blockchain) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "method not supported", http.StatusMethodNotAllowed)
+			return
+		}
+		to := r.URL.Query().Get("to")
+		from := r.URL.Query().Get("from")
+		amount := r.URL.Query().Get("amount")
+		amountInt, err := strconv.ParseInt(amount, 10, 32)
+		if err != nil {
+			log.Println("error converting string -> int: ", err)
+		}
+		newTrans := b.NewTransaction(from, to, amountInt)
+		transJSON, err := json.Marshal(newTrans)
+		if err != nil {
+			log.Println("error with JSON: ", err)
+		}
+		log.Println("new transaction: ", string(transJSON))
+		w.Write(transJSON)
+	})
 }
